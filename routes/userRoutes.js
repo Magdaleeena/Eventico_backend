6302f475983be6c0b533d5cc6248de9d74522c1c
Magdaleeena <magdalena.janeckova92@gmail.com>
2025-04-22 16:30:04 +0100

    clerk

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,16 +5,16 @@ const router = express.Router();
 const { authenticateClerkToken, isAdmin } = require('../middlewares/clerkAuthMiddleware');
 
 // Route to get all users - protected route
-router.get('/', authenticateClerkToken, isAdmin, userController.getAllUsers);
+router.get('/', ...authenticateClerkToken, isAdmin, userController.getAllUsers);
 
 // Route to get your profile - protected route
-router.get('/me', authenticateClerkToken, userController.getOwnProfile); 
+router.get('/me', ...authenticateClerkToken, userController.getOwnProfile); 
 
 // Route to update your profile - protected route
-router.put('/me', authenticateClerkToken, userController.updateOwnProfile); 
+router.put('/me', ...authenticateClerkToken, userController.updateOwnProfile); 
 
 // Route to delete your profile - protected route
-router.delete('/me', authenticateClerkToken, userController.deleteOwnProfile); 
+router.delete('/me', ...authenticateClerkToken, userController.deleteOwnProfile); 
 
 router.post("/sync", userController.syncUserFromClerk);
 
