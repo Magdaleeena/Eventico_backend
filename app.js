e9ca73a1d78c7a8d3557e9d836c47305460c1976
Magdaleeena <magdalena.janeckova92@gmail.com>
2025-04-22 16:02:42 +0100

    clerk

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const eventRoutes = require('./routes/eventRoutes');
 const endpointsRoutes = require('./routes/endpointsRoutes');
 const { mongooseValidationHandler, mongooseCastErrorHandler, customErrorHandler, serverErrorHandler } = require('./error-handlers');
 const path = require('path');
+const { ClerkExpressRequireAuth } = require('@clerk/express'); 
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(ClerkExpressRequireAuth());
 
 app.use('/api', endpointsRoutes);
 
